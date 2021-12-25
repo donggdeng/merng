f13bb17fe95a73ccffd29f0df5344a118f4caf66
Deng <donggdeng@gmail.com>
2021-12-25 13:00:24 +0800

    throw error when username is taken

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
@@ -8,12 +9,18 @@ module.exports = {
   Mutation: {
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
       // TODO: Calidate user data
       // TODO: Make sure user doesnt already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
       // TODO: hash password and create an auth token
       password = await bcrypt.hash(password, 12);
 
